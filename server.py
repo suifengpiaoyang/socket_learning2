bb2fcc921e19e2fbfb6dd99ff5d068cfe71d505f
suifengpiaoyang <504947985@qq.com>
2020-02-28 15:55:42 +0800

    version2:use nonblocking and update README.md

--- server.py
+++ server.py
@@ -7,14 +7,28 @@ The first version : use socket blocking
 address = ('127.0.0.1',9999)
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.bind(address)
+sock.setblocking(False)
 sock.listen(5)
+client_list = []
 print('Waitting for client connect...')
 
 while True:
-    s,addr = sock.accept()
-    print('Connected from {}'.format(addr))
-    data = s.recv(1024)
-    print('Recved data 【{}】 from {}'.format(data,addr[0]))
-    s.sendall(data)
-    print('Send data to the client.')
-    s.close()
+    try:
+        client,addr = sock.accept()
+        client_list.append(client)
+        client.setblocking(False)
+        print('Connected from {}'.format(addr))
+    except BlockingIOError:
+        pass
+
+    tmp_client_list = [line for line in client_list]
+    if len(tmp_client_list) != 0:
+        for client in tmp_client_list:
+            data = client.recv(1024)
+            print('Recved data 【{}】 from {}'.format(data,addr[0]))
+            client.sendall(data)
+            print('Send data【{}】 to the client.'.format(data))
+            client.close()
+            client_list.remove(client)
+    else:
+        pass
